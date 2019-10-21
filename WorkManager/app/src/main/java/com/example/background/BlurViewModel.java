51fde409b19c50098fa44dcfb2e42cffe92f8db2
VVat <kkww413@gmail.com>
2019-10-21 14:06:13 +0900

    Finish Worker Section 6

--- WorkManager/app/src/main/java/com/example/background/BlurViewModel.java
+++ WorkManager/app/src/main/java/com/example/background/BlurViewModel.java
@@ -16,18 +16,32 @@
 
 package com.example.background;
 
-import androidx.annotation.NonNull;
-import androidx.lifecycle.AndroidViewModel;
 import android.app.Application;
 import android.net.Uri;
 import android.text.TextUtils;
 
+import com.example.background.workers.BlurWorker;
+import com.example.background.workers.CleanupWorker;
+import com.example.background.workers.SaveImageToFileWorker;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.work.Data;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkContinuation;
+import androidx.work.WorkManager;
+
+import static com.example.background.Constants.KEY_IMAGE_URI;
+
 public class BlurViewModel extends AndroidViewModel {
 
     private Uri mImageUri;
-
+    private WorkManager mWorkManager;
     public BlurViewModel(@NonNull Application application) {
+
         super(application);
+        mWorkManager = WorkManager.getInstance(application);
+
     }
 
     /**
@@ -36,6 +50,44 @@ public class BlurViewModel extends AndroidViewModel {
      */
     void applyBlur(int blurLevel) {
 
+        WorkContinuation continuation =
+                mWorkManager.beginWith(OneTimeWorkRequest.from(CleanupWorker.class));
+
+        for (int i = 0; i < blurLevel; i++) {
+            OneTimeWorkRequest.Builder blurBuilder =
+                    new OneTimeWorkRequest.Builder(BlurWorker.class);
+
+            if ( i == 0 ) {
+                blurBuilder.setInputData(createInputDataForUri());
+            }
+
+            continuation = continuation.then(blurBuilder.build());
+        }
+
+        /*
+        OneTimeWorkRequest blurRequest = new OneTimeWorkRequest.Builder(BlurWorker.class)
+                .setInputData(createInputDataForUri())
+                .build();
+        continuation = continuation.then(blurRequest);
+        */
+
+        // Add WorkRequest to save the image to the filesystem
+        OneTimeWorkRequest save =
+                new OneTimeWorkRequest.Builder(SaveImageToFileWorker.class)
+                        .build();
+        continuation = continuation.then(save);
+
+        // Actually start the work
+        continuation.enqueue();
+
+        /*
+        OneTimeWorkRequest blurRequest =
+                new OneTimeWorkRequest.Builder(BlurWorker.class)
+                .setInputData(createInputDataForUri())
+                .build();
+        mWorkManager.enqueue(blurRequest);
+        */
+        //mWorkManager.enqueue(OneTimeWorkRequest.from(BlurWorker.class));
     }
 
     private Uri uriOrNull(String uriString) {
@@ -59,4 +111,13 @@ public class BlurViewModel extends AndroidViewModel {
         return mImageUri;
     }
 
+
+    private Data createInputDataForUri(){
+        Data.Builder builder = new Data.Builder();
+        if(mImageUri != null){
+            builder.putString(KEY_IMAGE_URI,mImageUri.toString());
+            return builder.build();
+        }
+        return builder.build();
+    }
 }
